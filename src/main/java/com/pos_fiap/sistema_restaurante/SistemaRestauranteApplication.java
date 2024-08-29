a61feebf2beaefb779ed872f225620a99d1f33ca
SUELITON DE OLIVEIRA (X01142591190) <x01142591190@tcu.gov.br>
2024-08-29 18:24:38 -0300

    gerando o projeto inicial

---
+++ src/main/java/com/pos_fiap/sistema_restaurante/SistemaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.pos_fiap.sistema_restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pos_fiap/sistema_restaurante/SistemaRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos_fiap.sistema_restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
